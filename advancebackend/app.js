c59bf6e567c7578a4c65dcbd01a0a5ce457a8148
Maaz-Afzaal <maazafzaal@gmail.com>
2022-09-11 13:09:16 +0500

    need to work on envirmoental variables

--- advancebackend/app.js
+++ advancebackend/app.js
@@ -3,8 +3,9 @@ const client = require('./config/postgressConfig');
 const dbSetup = require('./database/db');
 const User = require('./database/models/users');
 const router = require('./routes/router.js');
+require('dotenv').config();
+const port = process.env.ADVANCE_BACKEND_PORT || 8081;
 dbSetup();
-
 const app = express();
 app.use(express.json());
 app.use('/user', router);
@@ -20,4 +21,4 @@ app.get('/auth/:id', async (req, res) => {
 	}
 });
 
-app.listen(8081, () => console.log('server running on port 8081'));
+app.listen(port, () => console.log(`server running on port ${port}`));
